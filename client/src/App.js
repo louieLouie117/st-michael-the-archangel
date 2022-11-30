0c8d583d4bde0604009e1111c4cbbab70f506fd0
Luis Cardona <cardona-luis@outlook.com>
2022-11-30 11:30:41 -0600

    first-commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>St. Micheal The Archangel Website</h1>
+        <p>Designed by Luis Cardona</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
