b673117ce752067ac6283cf3e359db2e1d137dd6
Luis Cardona <cardona-luis@outlook.com>
2022-12-03 05:26:24 -0600

    MainMenuCompeted

--- client/src/components/LandingPage/MainNav.jsx
+++ client/src/components/LandingPage/MainNav.jsx
@@ -1,11 +1,74 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 
 function MainNav(props) {
+
+  const [MainMenu, setMainMenu] = useState(false)
+
+  const MainMenuHandler = e =>{
+    console.log("button was click")
+    if(MainMenu === false){
+      setMainMenu(true)
+    }else{
+      setMainMenu(false)
+    }
+    return
+  }
+
+
   return (
-    <div>
-      <h1>Welcome</h1>
-      <h3>Website is under development.</h3>
-      <h3>Please visit us another day.</h3>
+    <div className='MainNav-Component'>
+      <header>
+        <img  onClick={MainMenuHandler} src="/assets/IconMenu.png" alt="" />
+        <h1 onClick={MainMenuHandler}>Welcome</h1>
+      </header>
+
+      <div style={{display: MainMenu ? "grid": "none"}}>
+      <main className='NavList-Container'>
+        <ul>
+          <li>
+            <a href="/">Parish Staff</a>
+          </li>
+          <li>
+            <a href="/">Our Ministries</a>
+          </li>
+
+          <li>
+            <a href="/">Faith Formation</a>
+          </li>
+          <li>
+            <a href="/">Gift Shop & Books</a>
+          </li>
+       
+        </ul>
+
+        
+      </main>
+
+      <aside>
+        <ul className='contactUs-container'>
+          {/* <li>
+            <div>
+            <p>Phone Number:</p>
+            <p>(270) 640-9850</p>
+            </div>
+            <img src="/assets/iconPhone.png" alt="" />
+          </li> */}
+          <li>
+            <div>
+            <p>Address:</p>
+            <p>448 State Line Rd Oak Grove KY 42262</p>
+
+            </div>
+            <a href="http://maps.google.com/?q=448 State Line Rd Oak Grove KY 42262" target='blank'>
+            <img src="/assets/iconMapPin.png" alt=""  />
+            </a>
+          </li>
+        </ul>
+
+      </aside>
+      </div>
+     
     </div>
   )
 }
