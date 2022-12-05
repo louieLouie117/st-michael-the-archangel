6467bb651b1f99a27a8d0ac930a85955bf631e3b
Luis Cardona <cardona-luis@outlook.com>
2022-12-04 20:14:45 -0600

    AddToCalander

--- client/src/components/LandingPage/Announcements.jsx
+++ client/src/components/LandingPage/Announcements.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, {useState}from 'react'
 
 function Announcements(props) {
+  const [calendarOptions, setCalendarOptions] = useState(false);
+
+  const addToCalendarHandler = e =>{
+      console.log("button was click")
+      setCalendarOptions(true);
+  }
   return (
     <div className='Announcements-ViewPage'>
       <header>
@@ -16,13 +22,39 @@ function Announcements(props) {
               <main>
               <p>Wednesday December 14th 5:00pm</p>
               </main>
-            </li>
+              <footer>
+              
 
-
-           
+                <button onClick={addToCalendarHandler}>Add to Calendar</button>
+              </footer>
+            </li>           
           </ul>
 
+        
+
       </main>
+
+      <aside style={{display: calendarOptions ? "grid" : "none",
+                    left: calendarOptions ? "0px" : "-300px",
+                    transition: calendarOptions ? "smooth" : "smooth",
+                    transition: calendarOptions ? "30s" : "30s"}}>
+
+                <h2>Selected your calender</h2>
+              
+                <a href="https://outlook.office.com/calendar/0/deeplink/compose?allday=false&enddt=2022-12-15T00%3A00%3A00%2B00%3A00&path=%2Fcalendar%2Faction%2Fcompose&rru=Advent Reconciliation&startdt=2022-12-14T23%3A00%3A00%2B00%3A00&location=St Micheal The Archangel&subject=Advent Reconciliation" title="Save Event in my Calendar" target="blank">
+                  <button>Outlook</button>
+                </a>
+
+                <a href="https://calendar.google.com/calendar/render?action=TEMPLATE&text=Advent Reconciliation&dates=20221214T230000Z%2F20221215T000000Z" title="Save Event in my Calendar" target="blank">
+                  <button>Gmail</button>
+                </a>
+
+                <a href="https://calendar.yahoo.com/?dur=&et=20221215T000000Z&st=20221214T230000Z&v=60&&title=Advent Reconciliation" title="Save Event in my Calendar" target="blank">
+                  <button>Yahoo</button>
+                </a>
+
+                
+          </aside>
     </div>
   )
 }
