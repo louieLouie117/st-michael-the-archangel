eea2a5466538eb2c766ac67b73b4e8681f5f86a6
Luis Cardona <cardona-luis@outlook.com>
2022-12-07 06:10:07 -0600

    SacramentsComponent

--- client/src/components/LandingPage/Sacraments.jsx
+++ client/src/components/LandingPage/Sacraments.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Sacraments(props) {
   return (
-    <div>Sacraments</div>
+    <div className='Sacraments-Container'>
+        <ul>
+            <li>First Communion</li>
+            <li>Confirmation</li>
+            <li>Holy Order</li>
+            <li>Holy Matrimony</li>
+            <li>Anointing of the Sick</li>
+            <li>Reconciliation</li>
+            <li>Pray</li>
+        </ul>
+    </div>
   )
 }
 
